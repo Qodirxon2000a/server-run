6d7e7e3399678b4bf9927fede06ae869dc6f7fba
Qodirxon2000a <Ibshdh@gmail.com>
2024-10-24 15:39:59 +0500

    a

--- server.js
+++ server.js
@@ -1,23 +1,17 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
+const express = require("express");
+const connectDB = require("./config/db")
+const cors = require("cors")
+require("dotenv").config()
+const itemRoutes = require("./router/router")
+
+connectDB()
 const app = express();
-const productRoutes = require('./router/product.routes');
 
-app.use(cors());
-app.use(express.json());
-app.use('/uploads', express.static('uploads'));
+app.use(express.json())
+app.use(cors())
+app.use(express.urlencoded({ extended: false }))
 
-mongoose.connect(process.env.MONGO_URI, { 
-  useNewUrlParser: true, 
-  useUnifiedTopology: true 
-})
-  .then(() => console.log('MongoDB ulanish muvaffaqiyatli'))
-  .catch(err => console.error('MongoDB ulanishda xatolik', err));
+app.use("/api", itemRoutes)
 
-app.use('/api/products', productRoutes);
-
-const PORT = process.env.PORT || 5001;
-app.listen(PORT, () => {
-  console.log(`Server ${PORT} portda ishlayapti`);
-});
+const PORT = process.env.PORT || 3005
+app.listen(PORT, () => console.log(`Server ${PORT} portda ishga tushdi`))
